2db2e2ed93bc372ec5b76c3c3402d0f78c5cec39
PsyTheGuy <psyplaysgames@gmail.com>
2022-07-31 19:50:44 +0200

    Add function to run the game

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
+const choiceList = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
-    const choiceList = ['Rock', 'Paper', 'Scissors'];   // create a list of possible choices
-    const choiceIndex = Math.floor(Math.random() * 3);   // determines a random number (0,1 or 2)
-    return choiceList[choiceIndex]   // returns the randomly selected list item
+    //  roll a random number to determine choice (0, 1 or 2)
+    const choiceIndex = Math.floor(Math.random() * 3);
+    //  return the choice
+    return choiceList[choiceIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-    const choiceList = ['Rock', 'Paper', 'Scissors'];   // create a list of possible choices
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();   // handle case sensitivity
-    const playerWon = choiceList.indexOf(playerSelection) > choiceList.indexOf(computerSelection) || choiceList.indexOf(playerSelection) === 0 && choiceList.indexOf(computerSelection) === 2;   // determine if the player won
-    const isTie = choiceList.indexOf(playerSelection) === choiceList.indexOf(computerSelection);   // determine if the game is tied
-    if (playerWon) {
-        return `You Won! ${playerSelection} beats ${computerSelection}`;
-    } else if (isTie) {
-        return `It's a tie! Both selected ${playerSelection}`;
+    //  convert arguments to numbers
+    playerSelection = choiceList.indexOf(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
+    computerSelection = choiceList.indexOf(computerSelection);
+
+    //  create object to store results
+    let result = {};
+
+    //  determine the winner
+    if (playerSelection > computerSelection || playerSelection === 0 && computerSelection === 2) {
+        result.message = `You Won! ${choiceList[playerSelection]} beats ${choiceList[computerSelection]}`;
+        result.winner = "player";
+    } else if (playerSelection === computerSelection) {
+        result.message = `It's a tie! Both selected ${choiceList[playerSelection]}`;
+        result.winner = null;
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    };   // return who won the round
+        result.message = `You Lose! ${choiceList[computerSelection]} beats ${choiceList[playerSelection]}`;
+        result.winner = "computer";
+    };
+
+    return result;
+}
+
+function game() {
+
+    //  create variable to store player score
+    let playerScore = 0;
+    //  create variable to store computer score
+    let computerScore = 0;
+
+    //  loop trough 5 rounds of the game
+    for (let numberOfRounds = 0; numberOfRounds < 5; numberOfRounds++) {
+    //  get the player's choice and store it in a variable
+        let playerSelection = prompt('Choose one! (Rock, Paper or Scissors)');
+    //  get the computer's choice and store it in a variable
+        let computerSelection = getComputerChoice();
+    //  play the round and store the result in a variable
+        let result = playRound(playerSelection, computerSelection);
+    
+    //  announce the result of the round
+        console.log(result.message);
+    //  if player won increase score
+        if (result.winner == "player") {
+            playerScore += 1;
+        };
+        
+    //  if computer won increase score
+        if (result.winner == "computer") {
+            computerScore += 1;
+        };
+    };
+
+    //  announce the result of the game
+    if (playerScore > computerScore) {
+        console.log(`You won the game! Player: ${playerScore}, Computer: ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lost the game! Player: ${playerScore}, Computer: ${computerScore}`);
+    } else {
+        console.log(`It's a tie! Player: ${playerScore}, Computer: ${computerScore}`);
+    };
 }
