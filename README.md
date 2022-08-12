060bb8b0aaeb4502b8ff32fbe9a6565667c38f8f
PsyTheGuy <psyplaysgames@gmail.com>
2022-08-12 08:26:26 +0200

    add gui to the game

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # odin-rock-paper-scissors
 
 This is the Rock Paper Scissors project in the Fundamentals path of The Odin Project.
+
+Resources used for the project:
+
+ <a href="https://www.flaticon.com/free-icons/rock-paper-scissors" title="rock paper scissors icons">Rock paper scissors icons created by Freepik - Flaticon</a>
+
+ <a href="https://www.flaticon.com/free-icons/monitor" title="monitor icons">Monitor icons created by Freepik - Flaticon</a>
+
+ <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -1,71 +1,167 @@
-const choiceList = ['Rock', 'Paper', 'Scissors'];
+// controls the style of cards when clicked
+function selectCard() {
+  this.classList.add('selected');
+  setTimeout(() => this.classList.remove('selected'), 4000);
+}
 
+// turns off mouse pointer events on cards
+function disableCards(ms) {
+  cards.forEach(card => card.classList.add('disabled'));
+  setTimeout(() => cards.forEach(card => card.classList.remove('disabled')), ms);
+}
+
+// controls the opening and closing of the 'panel'
+function togglePanel(panel) {
+  if (panel.classList.contains('open')) {
+    window.setTimeout(() => panel.style.display = 'none', 1000);
+    panel.classList.toggle('open');
+  } else {
+    panel.style.display = 'block';
+    window.setTimeout(() => panel.classList.toggle('open'), 10);
+  };
+}
+
+// updates the 'text' appearing on the 'element'
+function updateText(element, text, ms=2000) {
+  if (element.classList.contains('visible')) {
+    element.classList.toggle('visible');
+    setTimeout(() => {
+      element.textContent = text;
+      element.classList.toggle('visible');
+    }, ms);
+  } else {
+    element.textContent = text;
+    element.classList.toggle('visible');
+    setTimeout(() => element.classList.toggle('visible'), ms);
+  };
+}
+
+// counts down from 'startNum' to 0 and updates 'inGameMessage'
+function countDown(startNum) {
+  let countDownTimer = startNum;
+  const intervalID = setInterval(() => {
+    if (countDownTimer > 0) {
+      updateText(inGameMessage, countDownTimer, 800);
+      countDownTimer--;
+    } else {
+      clearInterval(intervalID);
+    };
+  }, 1000);
+}
+
+// reveals the announcement overlay with 'text' content
+function overlayAnnounce(text) {
+  disableCards(3000);
+  overlayMessage.textContent = text;
+  overlay.style.display = 'block';
+  setTimeout(() => overlay.style.display = 'none', 3000);
+}
+
+// return a random choice
 function getComputerChoice() {
-    //  roll a random number to determine choice (0, 1 or 2)
-    const choiceIndex = Math.floor(Math.random() * 3);
-    //  return the choice
-    return choiceList[choiceIndex];
+  const choiceList = ['Rock', 'Paper', 'Scissors'];    
+  const choiceIndex = Math.floor(Math.random() * 3);
+  return choiceList[choiceIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-    //  convert arguments to numbers
-    playerSelection = choiceList.indexOf(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
-    computerSelection = choiceList.indexOf(computerSelection);
-
-    //  create object to store results
-    let result = {};
-
-    //  determine the result and save it to the result object
-    if (playerSelection > computerSelection || playerSelection === 0 && computerSelection === 2) {
-        result.message = `You Won! ${choiceList[playerSelection]} beats ${choiceList[computerSelection]}`;
-        result.winner = "player";
-    } else if (playerSelection === computerSelection) {
-        result.message = `It's a tie! Both selected ${choiceList[playerSelection]}`;
-        result.winner = null;
-    } else {
-        result.message = `You Lose! ${choiceList[computerSelection]} beats ${choiceList[playerSelection]}`;
-        result.winner = "computer";
-    };
-
-    //  return the result object
-    return result;
+// decide the winner of a round and update text accordingly
+function getWinner(playerSelection, computerSelection) {
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
+      playerSelection == 'Paper' && computerSelection == 'Rock' ||
+      playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    playerScore += 1;
+    updateText(inGameMessage, `You won the round! ${playerSelection} beats ${computerSelection.toLowerCase()}.`);
+    updateText(playerScoreText, playerScore, 200);
+  } else if (playerSelection === computerSelection) {
+    updateText(inGameMessage, `It's a tie! Both selected ${playerSelection.toLowerCase()}.`);
+  } else {
+    computerScore += 1;
+    updateText(inGameMessage, `Computer won the round! ${computerSelection} beats ${playerSelection.toLowerCase()}.`);
+    updateText(computerScoreText, computerScore, 200);
+  };
 }
 
-function game() {
+// control the events when the player plays a round
+function playRound() {
+  const playerSelection = this.innerText;
+  const computerSelection = getComputerChoice();
 
-    //  create variable to store player score
-    let playerScore = 0;
-    //  create variable to store computer score
-    let computerScore = 0;
-
-    //  loop trough 5 rounds of the game
-    for (let numberOfRounds = 0; numberOfRounds < 5; numberOfRounds++) {
-    //  get the player's choice and store it in a variable
-        let playerSelection = prompt('Choose one! (Rock, Paper or Scissors)');
-    //  get the computer's choice and store it in a variable
-        let computerSelection = getComputerChoice();
-    //  play the round and store the result in a variable
-        let result = playRound(playerSelection, computerSelection);
-    
-    //  announce the result of the round
-        console.log(result.message);
-    //  if player won increase score
-        if (result.winner == "player") {
-            playerScore += 1;
-        };
-        
-    //  if computer won increase score
-        if (result.winner == "computer") {
-            computerScore += 1;
-        };
-    };
-
-    //  announce the result of the game
-    if (playerScore > computerScore) {
-        console.log(`You won the game! Player: ${playerScore}, Computer: ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You lost the game! Player: ${playerScore}, Computer: ${computerScore}`);
-    } else {
-        console.log(`It's a tie! Player: ${playerScore}, Computer: ${computerScore}`);
-    };
+  disableCards(6500);
+  countDown(3);
+  setTimeout(getWinner, 4000, playerSelection, computerSelection);
+  (roundNumber == 5) ? setTimeout(endGame, 6500) : setTimeout(continueGame, 6500);
 }
+
+// control the events when the player starts the game
+function startGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNumber = 1;
+
+  updateText(playerScoreText, playerScore, 200);
+  updateText(computerScoreText, computerScore, 200);
+
+  togglePanel(preGamePanel);
+  setTimeout(() => {
+    togglePanel(scorePanel);
+    togglePanel(inGamePanel);
+  }, 1200);
+  setTimeout(overlayAnnounce, 2200, `Round: ${roundNumber}`);
+  
+  cards.forEach(card => card.addEventListener('click', playRound));
+  cards.forEach(card => card.addEventListener('click', selectCard));
+}
+
+// control the events if the current round was not the last
+function continueGame() {
+  roundNumber++; 
+  overlayAnnounce(`Round: ${roundNumber}`)
+}
+
+// control the events when the game ends
+function endGame() {
+  disableCards(4000);
+
+  if (playerScore > computerScore) {
+    overlayAnnounce('You won the game!');
+  } else if (computerScore > playerScore) {
+    overlayAnnounce('Computer won the game!');
+  } else {
+    overlayAnnounce('The game ended in a tie!');
+  };
+
+  setTimeout(() => {
+    togglePanel(scorePanel);
+    togglePanel(inGamePanel);    
+  }, 3000);
+  setTimeout(() => {
+    togglePanel(preGamePanel);
+  }, 4200);
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+// panel elements
+const scorePanel = document.getElementById('score-panel');
+const preGamePanel = document.getElementById('pregame-panel');
+const inGamePanel = document.getElementById('ingame-panel');
+// containers elements
+const scoreUI = document.getElementById('score-ui');
+const preGameUI = document.getElementById('pregame-ui');
+const inGameUI = document.getElementById('ingame-ui');
+// text elements
+const playerScoreText = document.getElementById('player-score');
+const computerScoreText = document.getElementById('computer-score');
+const inGameMessage = document.getElementById('ingame-msg');
+const overlayMessage = document.querySelector('.overlay-msg');
+// other ui elements
+const cards = document.querySelectorAll('.card');
+const startBtn = document.querySelector('.start-btn');
+const overlay = document.querySelector('.overlay');
+
+window.addEventListener('load', () => {
+  togglePanel(preGamePanel);
+  startBtn.addEventListener('click', startGame);
+}, 'once');
